4e7e2dc7576a072da139a5d875ad241056e85509
Francois-Xavier Lemire <francois-xavier.lemire@mail.mcgill.ca>
2016-04-25 16:01:21 -0400

    test structure

---
+++ test/test-app.js
@@ -0,0 +1,18 @@
+import {assert, helpers} from 'yeoman-generator';
+import path from 'path';
+
+describe('yang:app', () => {
+  describe('default', () => {
+    before(() => {
+
+    });
+
+    it('creates files', () => {
+
+    });
+
+    it('adds default ngapp', () => {
+
+    });
+  });
+});
